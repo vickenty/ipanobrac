20a0f36f173e573d4011dd0fbf7c25fbfcd0bc54
Vickenty Fesunov <kent@setattr.net>
2015-07-27 20:12:30 +0200

    Pass backend status code to the caller.

--- main.ml
+++ main.ml
@@ -7,19 +7,19 @@ let backend_uri = Uri.of_string "http://localhost:8080"
 
 let send_request uri =
   Client.get uri
-  >>= fun (code, body) -> Body.to_string body
-  >>= fun body -> return body
+  >>= fun (resp, body) -> Body.to_string body
+  >>= fun body -> return ((resp |> Response.status), body)
 
 let make_request path query =
   Uri.with_query (Uri.with_path backend_uri path) query
 
 let handler_metrics_find uri meth headers body =
   let req = make_request "/metrics/find/" (Uri.query uri) in
-  send_request req >>= fun resp -> return (`OK, resp)
+  send_request req
 
 let handler_render uri meth headers body =
   let req = make_request "/render/" (Uri.query uri) in
-  send_request req >>= fun resp -> return (`OK, resp)
+  send_request req
 
 let handler_not_found uri meth headers body =
   IO.return (`Not_found, "Bad luck: " ^ (Uri.path uri) ^ "\n")
