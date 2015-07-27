10a75a8e3fb4c5c8cab37fa57f9b39e0128898ce
Vickenty Fesunov <kent@setattr.net>
2015-07-27 19:27:44 +0200

    Pass requests to the backend.

--- main.ml
+++ main.ml
@@ -3,18 +3,46 @@ open Async.Std
 open Cohttp
 open Cohttp_async
 
-let make_response uri meth headers body =
-  Printf.sprintf "URI: %s\nMethod: %s\nHeaders: %s\nBody: %s\n" uri meth headers body
+let backend_uri = Uri.of_string "http://localhost:8080"
 
-let server =
-  let callback ~body _addr req =
-    let uri = req |> Request.uri |> Uri.to_string in
-    let meth = req |> Request.meth |> Code.string_of_method in
-    let headers = req |> Request.headers |> Header.to_string in
-    let resp_body = body |> Body.to_string >>=
-        (fun body -> return (make_response uri meth headers body)) in
-    resp_body >>= (fun body -> Server.respond_with_string body)
-  in
-  Server.create (Tcp.on_port 8085) callback
+let send_request uri =
+  Client.get uri
+  >>= fun (code, body) -> Body.to_string body
+  >>= fun body -> return body
+
+let make_request path query =
+  Uri.with_query (Uri.with_path backend_uri path) query
+
+let handler_metrics_find uri meth headers body =
+  let req = make_request "/metrics/find/" (Uri.query uri) in
+  send_request req >>= fun resp -> return (`OK, resp)
+
+let handler_render uri meth headers body =
+  let req = make_request "/render/" (Uri.query uri) in
+  send_request req >>= fun resp -> return (`OK, resp)
+
+let handler_not_found uri meth headers body =
+  IO.return (`Not_found, "Bad luck: " ^ (Uri.path uri) ^ "\n")
+
+let dispatch uri =
+  match Uri.path uri with
+  | "/metrics/find/" -> Some handler_metrics_find
+  | "/render/" -> Some handler_render
+  | _ -> None
+
+let make_response uri meth headers body =
+  match dispatch uri with
+  | Some handler -> handler uri meth headers body
+  | None -> handler_not_found uri meth headers body
+
+let callback ~body _addr req =
+  let uri = Request.uri req in
+  let meth = Request.meth req in
+  let headers = Request.headers req in
+  Body.to_string body
+  >>= fun body -> make_response uri meth headers body
+  >>= fun (code, body) -> Server.respond_with_string ~code body
+
+let server = Server.create (Tcp.on_port 8085) callback
 
 let () = never_returns (Scheduler.go ())
